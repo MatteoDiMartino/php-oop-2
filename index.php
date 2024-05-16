c848e79af74df3ae4e43f8fb07b75326ea51d700
MatteoDiMartino <matteodimartino359@gmail.com>
2024-05-16 17:40:00 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="card">
+        <img src="path/to/your/image.jpg" alt="Product Image" class="card-image">
+        <h2 class="card-title"></h2>
+        <p class="card-description"></p>
+    </div>
+
+
+</body>
+</html>
+
